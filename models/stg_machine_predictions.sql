84f49a3b20fc97fafbf0c0a458b61287f0bb16c2
Ajay Ghimire <102209654+Ajayghimire9@users.noreply.github.com>
2026-09-15 20:59:08 +0200

    feat: add dbt staging model

---
+++ models/stg_machine_predictions.sql
@@ -0,0 +1,10 @@
+select
+    model_version,
+    processing_time,
+    processing_time_std,
+    average_power_consumption,
+    average_power_consumption_std,
+    anomaly_score,
+    anomaly,
+    risk_level
+from {{ source('forgepulse', 'machine_predictions') }}
